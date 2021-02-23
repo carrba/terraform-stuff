362b0724fb9786e5e330c4ed3c29378a51c0aeab
carrba <b.carr@btinternet.com>
2021-02-23 16:31:53 +0000

    Updated vnet/variables.tf

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -1,8 +1,31 @@
-variable "resource_group_name" {
+# resource group variables
+variable "rg" {
     type = string
 }
 
 variable "location" {
     type = string
     default = "eastus"
+}
+
+# vnet variables
+variable "vnet" {
+    description = "vnet name"
+    type = string
+}
+
+variable "vnet_address" {
+    description = "vnet address space"
+    type = list(string)
+}
+
+# subnet variables
+variable "snet" {
+    description = "subnet name"
+    type = string
+}
+
+variable "snet_address" {
+    description = "subnet address prefixes"
+    type = list(string)
 }
