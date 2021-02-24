b6460cc0ae14a0bc8355b13d881c5f6222fa59ac
Brian Carr <b.carr@btinternet.com>
2021-02-24 23:22:24 +0000

    Added KeyVault

--- KeyVault/main.tf
+++ KeyVault/main.tf
@@ -10,7 +10,7 @@ terraform {
       resource_group_name   = "RG-terraformstate"
       storage_account_name  = "itbcvsterraformstorage"
       container_name        = "terraformdemo"
-      key                   = "module_vm_basic.terraform.tfstate"
+      key                   = "KeyVault.terraform.tfstate"
     }
 }
 
