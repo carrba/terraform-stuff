9cc2f5e297511b3fd9102bf204ef8801c6efbd38
Brian Carr <b.carr@btinternet.com>
2021-02-24 23:20:18 +0000

    Added KeyVault

---
+++ KeyVault/main.tf
@@ -0,0 +1,60 @@
+# terraform
+terraform {
+    required_providers {
+        azure = {
+            source  = "hashicorp/azurerm"
+            version = "=2.40.0"
+        }
+    }
+    backend "azurerm" {
+      resource_group_name   = "RG-terraformstate"
+      storage_account_name  = "itbcvsterraformstorage"
+      container_name        = "terraformdemo"
+      key                   = "module_vm_basic.terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+# create resource group
+resource "azurerm_resource_group" "rg" {
+    name = var.rg
+    location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "examplekeyvault"
+  location                    = var.location
+  resource_group_name         = var.rg
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
---
+++ KeyVault/variables.tf
@@ -0,0 +1,9 @@
+# resource group variables
+variable "rg" {
+    type = string
+}
+
+variable "location" {
+    type = string
+    default = "uksouth"
+}
