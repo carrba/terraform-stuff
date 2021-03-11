8ebb95ac6f86e2f899f281d6fed1ee847c90e5f9
carrba <b.carr@btinternet.com>
2021-03-11 12:56:27 +0000

    Splitting out provide/backend config into a separate file

--- vnet_basic/main.tf
+++ vnet_basic/main.tf
@@ -1,24 +1,3 @@
-# terraform
-terraform {
-  required_providers {
-    azure = {
-      source  = "hashicorp/azurerm"
-      version = "=2.40.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "RG-terraformstate"
-    storage_account_name = "itbcvsterraformstorage"
-    container_name       = "terraformdemo"
-    key                  = "vnet_basic.terraform.tfstate"
-  }
-}
-
-# azure provider
-provider "azure" {
-  features {}
-}
-
 # create resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.rg
