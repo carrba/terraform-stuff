7b8b8809e7267efb1cb29c60e6bb52daff16e189
Brian Carr <b.carr@btinternet.com>
2021-03-11 13:04:46 +0000

    Separated out backend/provider resource from main.tf to backend.tf

--- vm/main.tf
+++ vm/main.tf
@@ -1,19 +1,3 @@
-# terraform
-terraform {
-  required_providers {
-    azure = {
-      source  = "hashicorp/azurerm"
-      version = "=2.40.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "RG-terraformstate"
-    storage_account_name = "itbcvsterraformstorage"
-    container_name       = "terraformdemo"
-    key                  = "module_vm_basic.terraform.tfstate"
-  }
-}
-
 # azure provider
 provider "azure" {
   features {}
--- vm_basic/main.tf
+++ vm_basic/main.tf
@@ -1,19 +1,3 @@
-# terraform
-terraform {
-  required_providers {
-    azure = {
-      source  = "hashicorp/azurerm"
-      version = "=2.40.0"
-    }
-  }
-  backend "azurerm" {
-    resource_group_name  = "RG-terraformstate"
-    storage_account_name = "itbcterraformstorage"
-    container_name       = "terraformdemo"
-    key                  = "dev.terraform.tfstate"
-  }
-}
-
 # azure provider
 provider "azure" {
   features {}
