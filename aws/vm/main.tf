489f93f7097c1fc0ee512ea113b6f45d51ea9fee
Brian Carr <b.carr@btinternet.com>
2021-07-20 10:05:09 +0100

    Fixed security group ref

--- aws/vm/main.tf
+++ aws/vm/main.tf
@@ -13,13 +13,13 @@ provider "aws" {
 resource "aws_network_interface" "nic" {
   subnet_id = var.subnet_id
   # private_ips = var.ip_address
+  security_groups = [var.security_group_id]
 }
 
 resource "aws_instance" "ec2" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  security_groups = [var.security_group_id]
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
 
   network_interface {
     network_interface_id = aws_network_interface.nic.id
